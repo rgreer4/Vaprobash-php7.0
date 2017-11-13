bd11c3d3a3c6693be59ed683b3ed1f02bc9e9b21
Rick <rgreer4@users.noreply.github.com>
2017-11-13 13:18:21 -0600

    add local script subdir

--- scripts/server-script.sh
+++ scripts/server-script.sh
@@ -3,6 +3,7 @@
 echo ">>> Running remote share scripts"
 
 [[ -z $1 ]] && { echo "!!! Vagrant script folder not set, aborting."; exit 1; }
+[[ -z $2 ]] && { echo "!!! Vagrant local script folder not set, aborting."; exit 1; }
 
 sudo $1/local-script.sh
-sudo $1/localprivate-script.sh
+sudo $2/localprivate-script.sh
