9575d55f1b1c2f0affcbf5406b9d02b889db849b
Rick <rgreer4@users.noreply.github.com>
2017-11-13 13:17:59 -0600

    add local script subdir

--- scripts/server-script-nopriv.sh
+++ scripts/server-script-nopriv.sh
@@ -3,6 +3,7 @@
 echo ">>> Running remote share scripts"
 
 [[ -z $1 ]] && { echo "!!! Vagrant script folder not set, aborting."; exit 1; }
+[[ -z $2 ]] && { echo "!!! Vagrant local script folder not set, aborting."; exit 1; }
 
 $1/local-script-nopriv.sh
-$1/localprivate-script-nopriv.sh
+$2/localprivate-script-nopriv.sh
